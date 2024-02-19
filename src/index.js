61f1b00843b6f88110e4191c81a87c68158f25fd
kauderk <estudio1kau@gmail.com>
2024-02-19 03:20:16 -0500

    umd?

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import indexPage from './pages/index'
 
+console.log('indexPage', indexPage)
 export default indexPage
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 // @ts-check
 import { defineConfig } from 'vite'
 import marko from '@marko/vite'
+const name = 'LinklipExtension'
+const dev = process.env.NODE_ENV !== 'production'
+
 /**
  * @type {import('vite').UserConfigExport}
  */
@@ -13,5 +16,26 @@ export default defineConfig({
   build: {
     outDir: 'dist', // Server and client builds should output assets to the same folder.
     emptyOutDir: false, // Avoid server / client deleting files from each other.
+
+    sourcemap: true,
+    minify: false,
+    lib: {
+      entry: 'src/index.js',
+      formats: ['umd'],
+      name,
+      fileName: `${name}.js`,
+    },
+    rollupOptions: {
+      output: {
+        dir: 'dist',
+        name,
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name == 'style.css') return `${name}.css`
+          return `[name].[ext]`
+        },
+        entryFileNames: `${name}.js`,
+        chunkFileNames: `${name}.js`,
+      },
+    },
   },
 })
